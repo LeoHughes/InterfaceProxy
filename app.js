0e41da07960176de0e7894a59bbffe472d8d2ea2
leohughes <zhihy123@qq.com>
2017-06-08 15:41:08 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const path = require('path')
-const ProxyModel = require('./mod/ProxyModel')
+const path = require('path');
+const ProxyModel = require('./mod/ProxyModel');
 
 //根据接口配置文件 interface.json 地址初始化 ProxyModel
-let pm = new ProxyModel(path.resolve(__dirname, './interface.json'))
+let pm = new ProxyModel(path.resolve(__dirname, './interface.json'));
 
 //因为使用 async/await 所有获取接口数据操作要放在 async 方法下面
+
 (async() => {
 
   //获取单个接口数据  
