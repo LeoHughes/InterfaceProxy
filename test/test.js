737493e29ab9e435e25bea8a177cbdd0693b79cd
qianqc <qianqc@minstone.com.cn>
2018-03-08 17:28:42 +0800

    test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
  * @Author: Nico 
  * @Date: 2018-03-08 16:16:23 
  * @Last Modified by: Nico
- * @Last Modified time: 2018-03-08 17:16:43
+ * @Last Modified time: 2018-03-08 17:28:12
  */
 const assert = require('assert');
 const path = require('path');
@@ -12,16 +12,16 @@ describe('singleRequest', () => {
 
     let pm = new ProxyModel(path.resolve(__dirname, '../interface.json'));
 
-    // it('singleRequest -> response', async() => {
+    it('singleRequest -> response', async() => {
 
-    //     let response = await pm.send('getZhiHuData');
+        let response = await pm.send('getZhiHuData');
 
-    //     assert.ok(response != null);
-    // });
+        assert.ok(response != null);
+    });
 
     it('singleRequestExtend -> response', async() => {
 
-        let response = await pm.send('getZhiHuData', null, { q: '卫衣', code: 'utf-8' })
+        let response = await pm.send('getTaoBaoContent', null, { q: '卫衣', code: 'utf-8' })
 
         assert.ok(response != null);
 
